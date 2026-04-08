7739c7f92e3f28faa9e200efbda7ef01cdead3f7
redoy <redoy.hossen.emon@gmail.com>
2026-04-08 11:29:25 +0600

    add susspense

--- app/(root)/layout.jsx
+++ app/(root)/layout.jsx
@@ -1,16 +1,13 @@
 "use client";
-import { Suspense } from "react";
 import TopMenuBar from "@/app/component/application/menubar";
 
 export default function adminLayout({ children }) {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div className="w-full font-sans min-h-screen  flex flex-col items-center gap-16 m-auto">
-        <main className="w-full ">
-          <TopMenuBar />
-          {children}
-        </main>
-      </div>
-    </Suspense>
+    <div className="w-full font-sans min-h-screen  flex flex-col items-center gap-16 m-auto">
+      <main className="w-full ">
+        <TopMenuBar />
+        {children}
+      </main>
+    </div>
   );
 }
